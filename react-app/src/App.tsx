d60f2c0ecc0c1249edc84e9870df59e0dc608300
Mark Bartolo <6192527+lavarius@users.noreply.github.com>
2024-10-02 21:10:10 -0700

    Add visibilty of alert on button press

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <Button color="danger" onClick={() => console.log("Clicked")}>
+      {alertVisible && <Alert>My alert</Alert>}
+      <Button color="danger" onClick={() => setAlertVisibility(true)}>
         Push <span>Me</span>
       </Button>
     </div>
