7edb855dcc71bb7a7b1e565d4ebac64466500f80
Mark Bartolo <6192527+lavarius@users.noreply.github.com>
2024-10-03 22:21:22 -0700

    Remove react components for button

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,18 +1,9 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
 import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
-      )}
-      <Button color="danger" onClick={() => setAlertVisibility(true)}>
-        Push <span>Me</span>
-      </Button>
+      <Button onClick={() => {}}>My Button</Button>
     </div>
   );
 }
