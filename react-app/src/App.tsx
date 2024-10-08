faaa551056656531e582f6ad6214557939c4c50b
Mark Bartolo <6192527+lavarius@users.noreply.github.com>
2024-10-07 19:39:28 -0700

    Update pizza in exercise to include an additional topping and show it in console

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,28 @@
+import { useState, useEffect } from "react";
 import Button from "./components/Button/Button";
 
 function App() {
+  const [pizza, setPizza] = useState({
+    name: "Spicy Pepperoni",
+    toppings: ["Mushroom"],
+  });
+
+  useEffect(() => {
+    console.log("Current pizza:", pizza);
+  }, [pizza]);
+
+  const handleClick = () => {
+    console.log("handleClick called");
+    setPizza((prevPizza) => ({
+      ...prevPizza,
+      toppings: [...prevPizza.toppings, "Pineapple"],
+    }));
+  };
   return (
     <div>
-      <Button onClick={() => {}}>My Button</Button>
+      <h2>Pizza Details:</h2>
+      <pre>{JSON.stringify(pizza, null, 2)}</pre>
+      <Button onClick={handleClick}>Add Pineapple</Button>
     </div>
   );
 }
--- react-app/src/components/Button/Button.tsx
+++ react-app/src/components/Button/Button.tsx
@@ -1,19 +1,22 @@
 import styles from "./Button.module.css";
 
 interface Props {
-  children: string;
-  color?: "primary" | "secondary" | "danger" | "success";
+  children?: React.ReactNode;
+  // color?: "primary" | "secondary" | "danger" | "success";
   onClick: () => void;
 }
-const Button = ({ children, onClick, color = "primary" }: Props) => {
-  return (
-    <button
-      className={[styles.btn, styles["btn-" + color]].join(" ")}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
+// const Button = ({ children, onClick, color = "primary" }: Props) => {
+//   return (
+//     <button
+//       className={[styles.btn, styles["btn-" + color]].join(" ")}
+//       onClick={onClick}
+//     >
+//       {children}
+//     </button>
+//   );
+// };
+const Button: React.FC<Props> = ({ onClick, children }) => {
+  return <button onClick={onClick}>{children}</button>;
 };
 
 export default Button;
