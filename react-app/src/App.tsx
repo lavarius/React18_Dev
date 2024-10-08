341ba897e3b867ae1e93cfb8457a7bf819d224ee
Mark Bartolo <6192527+lavarius@users.noreply.github.com>
2024-10-07 19:53:10 -0700

    Update specific item in items array using map method.

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,27 +2,33 @@ import { useState, useEffect } from "react";
 import Button from "./components/Button/Button";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   useEffect(() => {
-    console.log("Current pizza:", pizza);
-  }, [pizza]);
+    console.log("Current cart:", cart);
+  }, [cart]);
 
   const handleClick = () => {
     console.log("handleClick called");
-    setPizza((prevPizza) => ({
-      ...prevPizza,
-      toppings: [...prevPizza.toppings, "Pineapple"],
+    setCart((cart) => ({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+      // arrow function and check ID of item, creat new object with updated qty otherwise retruun same item.
     }));
   };
   return (
     <div>
-      <h2>Pizza Details:</h2>
-      <pre>{JSON.stringify(pizza, null, 2)}</pre>
-      <Button onClick={handleClick}>Add Pineapple</Button>
+      <h2>Cart Details:</h2>
+      <pre>{JSON.stringify(cart, null, 2)}</pre>
+      <Button onClick={handleClick}>Change Quantity</Button>
     </div>
   );
 }
