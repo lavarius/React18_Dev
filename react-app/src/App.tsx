d617e39782b11af1bbda1d277ef476fe31160053
Mark Bartolo <6192527+lavarius@users.noreply.github.com>
2024-10-08 13:15:16 -0700

    Create first step of form to handle form submission

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,17 +1,12 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import Form from "./components/Form";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
-      )}
-      <Button color="danger" onClick={() => setAlertVisibility(true)}>
-        Push <span>Me</span>
-      </Button>
+      <Form />
     </div>
   );
 }
