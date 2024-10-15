42fa1344c506234653b0fdb6f7dd2d916446e6dd
Mark Bartolo <6192527+lavarius@users.noreply.github.com>
2024-10-15 09:19:11 -0700

    Use flex container on item and justify content between

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -38,10 +38,13 @@ function App() {
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
-      <ul>
+      <ul className="list-group">
         {users.map((user) => (
-          <li key={user.id}>
-            {user.name}{" "}
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
             <button className="btn btn-outline-danger">Delete</button>
           </li>
         ))}
